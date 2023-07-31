0d4925654ce80fb81a68ecc25df6e997b2b6d257
Sombrerito <furrfran@gmail.com>
2023-07-31 14:58:50 -0300

    Hicimo cositas

--- Programas/C/Hello.c
+++ Programas/C/Hello.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(){
-    printf("First C programm\n");
+    char a = 'c';                   //Single character %c
+    char b[] = "Bro";               //Array of characters %s
+    float c = 3.141592;             //4 bytes (32 of precision) %f
+    double d = 3.141592653589793;   // 8 Bytes (64 bits of precision) %lf
+    bool e = true;                  //1 byte (true or false) %d
+    char f = 120;                   //1 byte (-129 to +127) %
+    printf("hello %lf",d);
     return 0;
 }
