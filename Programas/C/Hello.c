8dd6c1b52457a34860df0f2ed8afcb3c54476586
Sombrerito <furrfran@gmail.com>
2023-08-03 13:11:53 -0300

    Moví cositas (Nada muy interesante)

--- Programas/C/Hello.c
+++ Programas/C/Hello.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 int main(){
-    char a = 'c';                   //Single character %c
-    char b[] = "Bro";               //Array of characters %s
-    float c = 3.141592;             //4 bytes (32 of precision) %f
-    double d = 3.141592653589793;   // 8 Bytes (64 bits of precision) %lf
-    bool e = true;                  //1 byte (true or false) %d
-    char f = 120;                   //1 byte (-129 to +127) %
-    printf("hello %lf",d);
+    printf("Hello world\n");
     return 0;
 }
