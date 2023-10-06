553680df42ceed3f2d8ba7200cec8ebad64007bd
Sombrero <furrfran@gmail.com>
2023-10-06 18:28:30 -0300

    Condicionales-a.c terminado; iniciado proximo.

--- Programas/Universidad/C/Proyecto 3/Ejercicios/Condicionales/condicionales-a.c
+++ Programas/Universidad/C/Proyecto 3/Ejercicios/Condicionales/condicionales-a.c
@@ -1,11 +1,41 @@
-#include <stdio.h>
-int main(int argc, char const *argv[])
-{
-    printf("Coloca el valor de x")
-    scanf("%d",&x)
-    printf("Coloca el valor de y")
-    scanf("%d",&y)
-    if x>=
-    return 0;
-}
+/* e) Var x, y : Int;
+[σ0 : (x → 3, y → 1)]
+if x ≥ y →
+[σ1:  . . . ] 
+x := 0
+[σ2:  . . . ] 
+x ≤ y →
+[σ'1:  . . .]
+x := 2
+[σ'2:  . . .]
+fi
+[σ3:  . . . ]  */
 
+
+
+#include <stdio.h>
+int main(void){
+    int x, y;
+    printf("Dale valor a x\n");
+    scanf("%d",&x);
+    printf("Dale valor a y\n");
+    scanf("%d",&y);
+    printf("Los valores colocados son x: %d y: %d\n",x,y);
+    if (x>=y)
+    {
+        x=0;
+        printf("El valor de x es mayor o igual al valor de y, por lo tanto transformamos a x en %d\n",x);
+    }
+    else{
+        x=2;
+        printf("El valor de y es mayor al valor de x, por lo tanto transformamos a x en %d\n",x);
+    }
+}
+/* 
+Dale valor a x
+3
+Dale valor a y
+1    
+Los valores colocados son x: 3 y: 1
+El valor de x es mayor o igual al valor de y, por lo tanto transformamos a x en 0
+*/
---
+++ Programas/Universidad/C/Proyecto 3/Ejercicios/Condicionales/condicionales-b.c
@@ -0,0 +1,26 @@
+/* 
+Var x, y : Int;
+σ0 : (x → −100, y → 1)
+if x ≥ y →
+σ1 : 
+x := 0
+σ2 : 
+x ≤ y →
+σ01: 
+x := 2
+σ02: 
+fi
+σ03:
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int x,y;
+    if (x>=y)
+    {
+        x=0/
+    }
+    
+}
