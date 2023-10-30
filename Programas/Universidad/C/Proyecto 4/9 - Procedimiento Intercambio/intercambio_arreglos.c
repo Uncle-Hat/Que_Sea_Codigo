2b0a6a17d05fdf080975e9e1273804139dde41a7
Sombrero <furrfran@gmail.com>
2023-10-30 11:59:35 -0300

    Actualización pre laboratorio

--- Programas/Universidad/C/Proyecto 4/9 - Procedimiento Intercambio/intercambio_arreglos.c
+++ Programas/Universidad/C/Proyecto 4/9 - Procedimiento Intercambio/intercambio_arreglos.c
@@ -16,58 +16,27 @@ Chequear que las posiciones estén en el rango correcto y luego imprimir en pant
 #include <stdio.h>
 #include <assert.h>
 
-void intercambiar(int tam, int a[], int i, int j)
-{
-    int aux;
-
-    printf("¿Qué lugar te gustaría cambiar de valor?\n");
-    scanf("%d", &i);
-    printf("¿Por cuál?\n");
-    scanf("%d", &j);
-
-    // Fijarse si i y j son válidos
-    if (i >= 0 && i < tam && j >= 0 && j < tam)
-    {
-        // Cambiar los elementos
-        aux = a[i];
-        a[i] = a[j];
-        a[j] = aux;
-        printf("Intercambio exitoso.\n");
-    }
-    else
-    {
-        printf("Índices inválidos.\n");
-    }
+void intercambiar(int tam, int a[], int i, int j){
+    
 }
 
-int pedir_arreglo(int tam, int a[])
+void pedir_arreglo(int tam, int a[])
 {
-
+    printf("Coloque el tamaño máximo del arreglo: \n");
+    scanf("%d", &tam);
     printf("Ingrese los valores del arreglo:\n");
-    for (int lambda = 0; lambda < tam; lambda++)
+    for (int i = 0; i < tam; i++)
     {
-        printf("a[%d] = ", lambda);
-        scanf("%d", &a[lambda]);
+        printf("a[%d] = ", i);
+        scanf("%d", &a[i]);
     }
-    return 0;
+    return tam,a;
 }
 
 int main()
 {
-    int tam;
-    printf("Coloque el tamaño máximo del arreglo: \n");
-    scanf("%d", &tam);
-    assert(tam > 0);
+    int tam,a,i,j;
+    pedir_arreglo(tam,a);
     int a[tam];
-    pedir_arreglo(tam, a);
-    int i, j;
-    intercambiar(tam, a, i, j);
-
-    printf("El arreglo después del intercambio:\n");
-    for (int lambda = 0; lambda < tam; lambda++)
-    {
-        printf("a[%d] = %d\n", lambda, a[lambda]);
-    }
-
     return 0;
 }
